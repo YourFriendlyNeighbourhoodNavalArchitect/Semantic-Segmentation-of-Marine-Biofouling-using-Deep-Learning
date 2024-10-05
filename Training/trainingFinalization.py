e71967e8d948e1b6f2494c1f9f24bfbedab55c2f
Ioannis Karlatiras <104106792+YourFriendlyNeighbourhoodNavalArchitect@users.noreply.github.com>
2024-10-05 20:43:21 +0300

    Update trainingFinalization.py

--- Training/trainingFinalization.py
+++ Training/trainingFinalization.py
@@ -2,20 +2,35 @@ import os
 import torch
 from json import dump
 
-def saveONNX(model, device, inputShape, savePath):
+def saveONNX(model, device, inputShape, savePath, trialNumber):
         # ONNX offers framework interoperability and shared optimization [https://en.wikipedia.org/wiki/Open_Neural_Network_Exchange].
         # Exporting requires dummy input tensor.
         dummyInput = torch.randn(inputShape).to(device)
-        path = os.path.join(savePath, 'bestModelOverall.onnx')
+        path = os.path.join(savePath, f'modelTrial{trialNumber}.onnx')
         # Constant folding improves efficiency.
         torch.onnx.export(model, dummyInput, path, export_params = True, 
                           opset_version = 12, do_constant_folding = True,
                           input_names = ['Input'], output_names = ['Output'])
-        print(f"Best model saved in ONNX format at {path}.")
+        print(f"Model for trial {trialNumber} saved in ONNX format at {path}.")
+        return path
 
 def saveBestHyperparameters(trial, savePath):
         # Fetch optimal hyperparameters in .json format for future reference.
-        path = os.path.join(savePath, 'bestHyperparameters.json')
+        path = os.path.join(savePath, f'hyperparametersTrial{trial.number}.json')
         with open(path, 'w') as f:
             dump(trial.params, f, indent = 4)
-        print(f"Hyperparameters for best model saved at {path}.")
+        print(f"Hyperparameters for trial {trial.number} saved at {path}.")
+        return path
+
+def deleteResiduals(savedFiles, bestTrialNumber, modelSavePath):
+    bestModelFile = os.path.join(modelSavePath, f'modelTrial{bestTrialNumber}.onnx')
+    bestHyperparametersFile = os.path.join(modelSavePath, f'hyperparametersTrial{bestTrialNumber}.json')
+
+    for ONNXFile, JSONFile in savedFiles:
+        if ONNXFile != bestModelFile:
+            os.remove(ONNXFile)
+        if JSONFile != bestHyperparametersFile:
+            os.remove(JSONFile)
+
+    os.rename(bestModelFile, os.path.join(modelSavePath, 'bestModel.onnx'))
+    os.rename(bestHyperparametersFile, os.path.join(modelSavePath, 'bestHyperparameters.json'))
