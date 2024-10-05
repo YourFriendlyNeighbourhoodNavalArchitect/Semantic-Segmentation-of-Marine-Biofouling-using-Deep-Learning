4b9ffcbfb00956df18a48a2abee041c99ceab3da
Ioannis Karlatiras <104106792+YourFriendlyNeighbourhoodNavalArchitect@users.noreply.github.com>
2024-10-05 16:36:41 +0300

    Update DatasetVisualizer.py

--- Dataset/DatasetVisualizer.py
+++ Dataset/DatasetVisualizer.py
@@ -1,42 +1,52 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from ImageDataset import ImageDataset
 
 class DatasetVisualizer:
-    def __init__(self, rootPath, augmentationFlag, visualizationFlag, numSamples):
+    def __init__(self, rootPath, augmentationFlag):
         # Inputs to ImageDataset.py.
         self.rootPath = rootPath
         self.augmentationFlag = augmentationFlag
-        self.visualizationFlag = visualizationFlag
-        self.numSamples = numSamples
+
         self.dataset = self.loadDataset()
         self.currentIndex = 0
-        self.fig, self.axs = plt.subplots(1, 2, figsize = (10, 5))
+        self.figure, self.axes = plt.subplots(1, 2, figsize = (10, 5))
         self.updatePlot()
+
         # Move right or left using keyboard arrows, as per method below.
-        self.fig.canvas.mpl_connect('key_press_event', self.onKeyPress)
+        self.figure.canvas.mpl_connect('key_press_event', self.onKeyPress)
         plt.show()
 
     def loadDataset(self):
         # Instantiation of ImageDataset.
-        dataset = ImageDataset(self.rootPath, self.augmentationFlag, self.visualizationFlag)
-        print(f"Total number of samples in the dataset: {len(dataset)}")
-        return dataset
+        dataset = ImageDataset(self.rootPath, self.augmentationFlag)
+        if len(dataset) == 0:
+            raise ValueError("No data pairs located.")
+        else:
+            print(f"Total number of samples in the dataset: {len(dataset)}")
+            return dataset
 
-    def updatePlot(self):
-        # Utilizing Pyplot, print image and mask side by side.
-        image, mask = self.dataset[self.currentIndex]
-        title = f"Sample {self.currentIndex + 1}"
-        
-        self.axs[0].imshow(image.permute(1, 2, 0).numpy())
-        self.axs[0].set_title("Image")
-        self.axs[0].axis("off")
+    def classIndicesToRGB(self, mask):
+        # Create a colorized version of the mask.
+        # Green for 'No fouling'.
+        # Light yellow for 'Light fouling'.
+        # Red for 'Heavy fouling'.
+        # Blue for 'Sea'.
+        # Black for irrelevant areas, arising from augmentation.
+        classColours = {
+            1: [0, 255, 0],
+            2: [255, 255, 102],
+            3: [255, 0, 0],
+            4: [0, 0, 255]
+        }
 
-        self.axs[1].imshow(mask.permute(1, 2, 0).numpy())
-        self.axs[1].set_title("Mask")
-        self.axs[1].axis("off")
-
-        self.fig.canvas.manager.set_window_title(title)
-        plt.draw()
+        # Create an empty RGB image for the colorized mask.
+        mask = mask.squeeze(0).numpy()
+        height, width = mask.shape
+        RGBMask = np.zeros((height, width, 3), dtype = np.uint8)
+        for classIndex, colour in classColours.items():
+            RGBMask[mask == classIndex] = colour
+        return RGBMask
 
     def onKeyPress(self, event):
         if event.key == 'right':
@@ -45,10 +55,27 @@ class DatasetVisualizer:
         elif event.key == 'left':
             self.currentIndex = (self.currentIndex - 1) % len(self.dataset)
             self.updatePlot()
+        else:
+            pass
+
+    def updatePlot(self):
+        # Utilizing Pyplot, print image and mask side by side.
+        image, mask = self.dataset[self.currentIndex]
+        title = f"Sample {self.currentIndex + 1}"
+        
+        self.axes[0].imshow(image.permute(1, 2, 0).numpy())
+        self.axes[0].set_title("Image")
+        self.axes[0].axis("off")
+
+        RGBMask = self.classIndicesToRGB(mask)
+        self.axes[1].imshow(RGBMask)
+        self.axes[1].set_title("Mask")
+        self.axes[1].axis("off")
+
+        self.figure.canvas.manager.set_window_title(title)
+        plt.draw()
 
     
 rootPath = r"C:\Users\giann\Desktop\NTUA\THESIS\Thesis\INPUTS"
 augmentationFlag = True
-visualizationFlag = True
-numSamples = 5
-DatasetVisualizer(rootPath, augmentationFlag, visualizationFlag, numSamples)
+DatasetVisualizer(rootPath, augmentationFlag)
