1e7ccf73b048b71e1ec47fa36ceeeae532ad0c06
Ioannis Karlatiras <104106792+YourFriendlyNeighbourhoodNavalArchitect@users.noreply.github.com>
2024-10-05 20:25:44 +0300

    Update videoToFrames.py

--- Dataset/videoToFrames.py
+++ Dataset/videoToFrames.py
@@ -13,11 +13,11 @@ def videoToFrames(pathIn, pathOut, frameRate):
         video.set(cv2.CAP_PROP_POS_MSEC, (count * 1000 * frameRate))
         success, image = video.read()
         if success:
-            print(f'Read a new frame at {count * frameRate} seconds.')
+            print(f"Read a new frame at {count * frameRate} seconds.")
             cv2.imwrite(os.path.join(pathOut, f'frame{count}.jpg'), image)
             count += 1
         else:
-            print('End of video or error encountered.')
+            print(f"End of video or error encountered.")
 
 pathIn = r'C:\Users\giann\Desktop\NTUA\THESIS\Thesis\INPUTS\Videos\Cage\Cage.MP4'
 # New images are saved in the same path as the original video.
