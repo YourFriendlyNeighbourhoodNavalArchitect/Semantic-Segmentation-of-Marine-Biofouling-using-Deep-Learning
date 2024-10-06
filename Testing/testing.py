1c2901f1bb94d2168f1c275ea4f07bc8ac290222
Ioannis Karlatiras <104106792+YourFriendlyNeighbourhoodNavalArchitect@users.noreply.github.com>
2024-10-06 15:09:34 +0300

    Update testing.py

--- Testing/testing.py
+++ Testing/testing.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from onnxruntime import InferenceSession
-from torchvision import transforms
 from PIL import Image
+from torchvision import transforms
 from trainingInitialization import setupDevice
 
 def classIndicesToRGB(prediction):
@@ -25,35 +25,39 @@ def classIndicesToRGB(prediction):
         RGBMask[prediction == classIndex] = colour
     return RGBMask
 
-def testModel(modelPath, imagePath, device):
+def testModel(modelPath, testPath, device):
     # Create ONNX Runtime session.
     providers = ['CUDAExecutionProvider', 'CPUExecutionProvider'] if device == 'cuda' else ['CPUExecutionProvider']
     session = InferenceSession(modelPath, providers = providers)
 
-    transform = transforms.Compose([
-        transforms.Resize((256, 256)),
-        transforms.ToTensor(),
-    ])
+    transform = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()])
     
     # Simple error handling.
     try:
-        image = Image.open(imagePath).convert('RGB')
-        image = transform(image).unsqueeze(0).numpy()
-        image = image.astype(np.float32)
+        image = Image.open(testPath).convert('RGB')
+        image = transform(image).unsqueeze(0).numpy().astype(np.float32)
         output = session.run(None, {session.get_inputs()[0].name: image})[0]
         prediction = np.argmax(output[:, 1:], axis = 1).squeeze() + 1
     except Exception as e:
-        print(f"Error loading image: {e}.")
+        print(f"Error: {e}.")
         return
 
     RGBMask = classIndicesToRGB(prediction)
+    plt.figure(figsize = (10, 5))
 
+    plt.subplot(1, 2, 1)
+    plt.imshow(np.transpose(image.squeeze(), (1, 2, 0)))
+    plt.title("Image")
+    plt.axis('off')
+
+    plt.subplot(1, 2, 2)
     plt.imshow(RGBMask)
     plt.title("Predicted mask")
     plt.axis('off')
+
     plt.show()
 
 modelPath = r'C:\Users\giann\Desktop\NTUA\THESIS\Thesis\OUTPUTS\Trained model\bestModel.onnx'
-imagePath = r'C:\Users\giann\Desktop\NTUA\THESIS\Thesis\INPUTS\1. Slime\1.15.jpg'
+testPath = r'C:\Users\giann\Desktop\NTUA\THESIS\Thesis\INPUTS\TESTING\Images\15.jpg'
 device = setupDevice()
-testModel(modelPath, imagePath, device)
+testModel(modelPath, testPath, device)
