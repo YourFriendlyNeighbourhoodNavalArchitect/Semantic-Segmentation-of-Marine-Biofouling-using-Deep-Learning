b868865273f6666650db7080fa0c80a2e212b4de
Ioannis Karlatiras <104106792+YourFriendlyNeighbourhoodNavalArchitect@users.noreply.github.com>
2024-12-30 15:23:02 +0200

    Add files via upload

--- Various/visualizeModel.py
+++ Various/visualizeModel.py
@@ -5,16 +5,16 @@ from torchsummary import summary
 from UNet import UNet
 from initializeWeights import initializeWeights
 from trainingInitialization import setupDevice
-from configurationFile import NUM_CLASSES, VISUALISATIONS_PATH
+from configurationFile import NUM_CLASSES, VISUALISATIONS_PATH, RESOLUTION
 
 # Dummy script to visualize the network and confirm its structure and output form.
 device = setupDevice()
 # Segmentation is performed in various different classes, as explained in configurationFile.py.
 model = UNet(inChannels = 3, numClasses = NUM_CLASSES).to(device)
 model.apply(initializeWeights)
-summary(model, input_size = (3, 256, 256))
+summary(model, input_size = (3, *RESOLUTION))
 
-dummyInput = randn(1, 3, 256, 256).to(device)
+dummyInput = randn(1, 3, *RESOLUTION).to(device)
 output = model(dummyInput)
 print(output.shape)
 
