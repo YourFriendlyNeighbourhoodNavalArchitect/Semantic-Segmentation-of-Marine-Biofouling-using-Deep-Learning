ea9bb4dab6ad90e2e96308e6073a74325760814a
Ioannis Karlatiras <104106792+YourFriendlyNeighbourhoodNavalArchitect@users.noreply.github.com>
2025-01-17 19:07:19 +0200

    Add files via upload

--- Various/visualizeModel.py
+++ Various/visualizeModel.py
@@ -2,23 +2,29 @@ from torch import randn
 from os.path import join
 from torchviz import make_dot
 from torchsummary import summary
-from UNet import UNet
+from trainingInitialization import initializeModel
 from initializeWeights import initializeWeights
 from trainingInitialization import setupDevice
-from configurationFile import NUM_CLASSES, VISUALISATIONS_PATH, RESOLUTION
+from configurationFile import NUM_CLASSES, VISUALIZATIONS_PATH, RESOLUTION
 
-# Dummy script to visualize the network and confirm its structure and output form.
-device = setupDevice()
-# Segmentation is performed in various different classes, as explained in configurationFile.py.
-model = UNet(inChannels = 3, numClasses = NUM_CLASSES).to(device)
-model.apply(initializeWeights)
-summary(model, input_size = (3, *RESOLUTION))
+def visualizeModel(modelFlag):
+    # Dummy script to visualize the network and confirm its structure and output form.
+    device = setupDevice()
+    # Segmentation is performed in various different classes, as explained in configurationFile.py.
+    model = initializeModel(modelFlag = modelFlag, inChannels = 3, numClasses = NUM_CLASSES, device = device)
+    model.apply(initializeWeights)
+    summary(model, input_size = (3, *RESOLUTION))
 
-dummyInput = randn(1, 3, *RESOLUTION).to(device)
-output = model(dummyInput)
-print(output.shape)
+    dummyInput = randn(1, 3, *RESOLUTION).to(device)
+    output = model(dummyInput)
+    print(output.shape)
+    dot = make_dot(output, params = dict(model.named_parameters()))
+    
+    if modelFlag:
+        path = join(VISUALIZATIONS_PATH, 'U-Net Architecture Graph')
+    else:
+        path = join(VISUALIZATIONS_PATH, 'Simple CNN Architecture Graph')
+    outputPath = dot.render(path, format = 'png', view = True)
+    print(f'File saved to {outputPath}.')
 
-dot = make_dot(output, params = dict(model.named_parameters()))
-path = join(VISUALISATIONS_PATH, 'Architecture graph')
-outputPath = dot.render(path, format = 'png', view = True)
-print(f'File saved to {outputPath}.')
+visualizeModel(False)
